ea5a5e9ce70f81ae01d74885fb4d2a1d0492cc8f
Mirko <mirko.lana@skiff.com>
2023-09-17 14:57:37 +0200

    Added CLI Arument Parser

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 mod turing_machine;
 
 use std::io;
+use clap::Parser;
 use crate::turing_machine::TuringMachine;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    #[arg(short,
+          long,
+          value_name="CONF_PATH",
+          help="TMS configuration file path"
+    )]
+    conf: String,
+}
+
 fn main() -> Result<(), io::Error> {
-    let cfg_path = "/Users/mirko/Projects/tms/examples/example.cfg";
+    let args = Args::parse();
+    let conf = args.conf;
     let mut tm = TuringMachine::new();
     
-    if let Err(error) = tm.load_cfg(cfg_path) {
+    if let Err(error) = tm.load_cfg(&conf) {
         panic!("Error: {}", error);
     }
 
