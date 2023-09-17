84fc6ca5e64f12d0ff379addd296c8d114a582fa
Mirko Lana <mirko@Mirkos-MacBook-Air.local>
2023-09-17 02:08:29 +0200

    Better `README.md` and Split `main.rs` into Multiple Files

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-
 # Turing Machine Simulator - TMS
 
 An Alan Turing machine simulator written in Rust as part of my personal learning journey to master the Rust programming language.
-
 
 <!--
 
@@ -10,15 +8,58 @@ An Alan Turing machine simulator written in Rust as part of my personal learning
 
 ![App Screenshot]()
 
-## Installation
-
-TODO
-    
-## Usage/Examples
-
-TODO
-
 -->
+
+## Getting Started
+
+Follow these instructions to get the Turing Machine simulator up and running on your system.
+
+### Prerequisites
+
+Before you begin, ensure you have the following installed:
+
+- [Rust](https://www.rust-lang.org/tools/install) - The Rust programming language.
+
+### Installation
+
+1. Clone the repository:
+
+    ```bash
+    $ git clone https://github.com/LanaMirko04/tms
+    $ cd tms
+    ```
+
+2. Build the project:
+
+    ```bash
+    $ cargo build
+    ```
+
+3. Run the program:
+
+    ```bash
+    $ cargo run
+    ```
+
+## Usage
+
+**Important Note:** The command-line interface (CLI) for this Turing Machine simulator has not been implemented yet. To use the simulator, you will need to manually modify the path to the configuration file within the source code.
+
+To load your configuration file, follow this steps:
+
+1. Open `src/main.rs` file in the project directory.
+2. Locate the following line of code:
+
+    ```rust
+    let cfg_path = "/Users/mirko/Projects/tms/examples/example.cfg";
+    ```
+
+3. Modify the `cfg_path` variable and save the changes.
+4. Run the program:
+
+    ```bash
+    $ cargo run
+    ```
 
 ## License
 
@@ -27,6 +68,6 @@ TMS is licensed under the MIT License. See the [LICENSE](LICENSE) file for more 
 
 ## Acknowledgements
 
- - [The Rust Book](https://doc.rust-lang.org/stable/book/)
+ - [The Rust Programming Language Book](https://doc.rust-lang.org/stable/book/)
  - [The Cargo Book](https://doc.rust-lang.org/stable/cargo/)
 
--- src/main.rs
+++ src/main.rs
@@ -1,159 +1,7 @@
-use std::fs::File;
-use std::io::{self, BufRead};
+mod turing_machine;
 
-#[derive(Copy, Clone, Debug)]
-enum Direction {
-    LHS,
-    RHS,
-    STAY,
-}
-
-impl Direction {
-    fn str2dir(strdir: &str) -> Result<Direction, io::Error> {
-        match strdir {
-            "left" => Ok(Direction::LHS),
-            "right" => Ok(Direction::RHS),
-            "stay" => Ok(Direction::STAY),
-            _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid direction")),
-        }
-    }
-
-    fn dir2str(dir: &Direction) -> String {
-        match dir {
-            Direction::LHS => "left".to_string(),
-            Direction::RHS => "right".to_string(),
-            Direction::STAY => "stay".to_string(),
-        }
-    }
-}
-
-struct Instruction {
-    current_state: String,
-    current_symbol: char,
-    new_state: String,
-    new_symbol: char,
-    direction: Direction,
-}
-
-impl Instruction {
-    fn is_matching(&self, state: &str, symbol: char) -> bool {
-        return state == self.current_state && symbol == self.current_symbol
-    }
-
-    fn to_string(&self) -> String {
-        format!("instruction {{current_state: {}, current_symbol: {}, new_state: {}, new_symbol: {}, direction: {}}}",
-                self.current_state,
-                self.current_symbol,
-                self.new_state,
-                self.new_symbol,
-                Direction::dir2str(&self.direction))
-    }
-}
-
-pub struct TuringMachine {
-    state: String,
-    halt_state: String,
-    tape: Vec<char>,
-    tape_cell: usize,
-    instructions: Vec<Instruction>,
-}
-
-impl TuringMachine {
-    pub fn new() -> Self {
-        Self {
-            state: String::from(""),
-            halt_state: String::from(""),
-            tape: Vec::new(),
-            tape_cell: 0,
-            instructions: Vec::new(),
-        }
-    }
-
-    pub fn load_cfg(&mut self, path: &str) -> Result<(), io::Error> {
-        let file = File::open(path).unwrap(); 
-        let reader = io::BufReader::new(file);
-
-        for line in reader.lines() {
-            let line = line?;
-            if line.starts_with('#') || line.trim().is_empty() {
-                continue;
-            }
-
-            let substrings: Vec<&str> = line.split_whitespace().collect();
-            let count = substrings.len();
-
-            match count {
-                2 => {
-                    self.state = substrings[0].to_string();
-                    self.tape = substrings[1].chars().collect();
-                }
-                1 => {
-                    self.halt_state = substrings[0].to_string();
-                }
-                5 => {
-                    let current_state = substrings[0].to_string();
-                    let current_symbol = substrings[1].chars().next().unwrap();
-                    let new_state = substrings[2].to_string();
-                    let new_symbol = substrings[3].chars().next().unwrap();
-                    let direction = Direction::str2dir(substrings[4])
-                        .unwrap();
-
-                    self.instructions.push(Instruction {
-                        current_state,
-                        current_symbol,
-                        new_state,
-                        new_symbol,
-                        direction,
-                    });
-                }
-                _ => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "Invalid line in CFG file.",
-                    ));
-                }
-            }
-        }
-
-        Ok(())
-    }
-
-    pub fn step(&mut self) {
-        for i in 0..self.instructions.len() {
-            if self.instructions[i]
-                .is_matching(&self.state, self.tape[self.tape_cell]) {
-                let state: String = self.instructions[i].new_state.clone();
-                let symbol: char = self.instructions[i].new_symbol;
-                let direction: Direction = self.instructions[i].direction;
-
-                self.update(&state, symbol, direction); 
-                break;
-            }
-        }
-    }
-
-    pub fn print_info(&self) {
-        println!("State: {}", self.state);
-        println!("Halt state: {}", self.halt_state);
-        println!("Tape: {:?}", self.tape);
-        println!("Tape cell: {}", self.tape_cell);
-        println!("Instructions:");
-        for (i, instruction) in self.instructions.iter().enumerate() {
-            println!("#{}: {}", i + 1, instruction.to_string());
-        }
-    }
-
-    fn update(&mut self, new_state: &str, new_symbol: char, dir: Direction) {
-        self.state = new_state.to_string();
-        self.tape[self.tape_cell] = new_symbol;
-
-        match dir {
-            Direction::LHS => self.tape_cell -= 1,
-            Direction::RHS => self.tape_cell += 1,
-            Direction::STAY => {},
-        }
-    }
-}
+use std::io;
+use crate::turing_machine::TuringMachine;
 
 fn main() -> Result<(), io::Error> {
     let cfg_path = "/Users/mirko/Projects/tms/examples/example.cfg";
@@ -161,6 +9,12 @@ fn main() -> Result<(), io::Error> {
     
     if let Err(error) = tm.load_cfg(cfg_path) {
         panic!("Error: {}", error);
+    }
+
+    tm.print_tape();
+    while !tm.is_halt() {
+        tm.step();
+        tm.print_tape()
     }
 
     Ok(())
